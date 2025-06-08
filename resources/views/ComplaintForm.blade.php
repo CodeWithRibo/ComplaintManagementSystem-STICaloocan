2aa5c479068216d89847e025746b3b1d71ecb152
CodeWithRibo <riboluna2818@gmail.com>
2025-06-08 21:51:11 +0800

    Added Validation

--- resources/views/ComplaintForm.blade.php
+++ resources/views/ComplaintForm.blade.php
@@ -1,28 +1,46 @@
 <x-layout>
-    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <form action="{{route('store')}}" method="POST">
+    <form action="{{route('store')}}" method="POST">
+        <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
             @csrf
             @method('POST')
+
             <legend class="fieldset-legend">Submit Complaint</legend>
-            <input type="text"  name="title" class="input" placeholder="Title" />
-            <input type="text" name="description" class="input" placeholder="Description" />
-            <input type="datetime-local" name="timeIncident" class="input" placeholder="Date and Time of Incident" />
+            <input type="text" name="title" class="input @error('title') @enderror" placeholder="Title" value="{{ old('title') }}" />
+            @error('title')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <input type="text" name="description" class="input" placeholder="Description" value="{{ old('description') }}" />
+            @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <input type="datetime-local" name="timeIncident" class="input" placeholder="Date and Time of Incident" value="{{ old('timeIncident') }}" />
+            @error('timeIncident')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
             <select name="categorySelection" class="input">
-                <option value="" disabled selected>Select Category</option>
-                <option value="Admission">Admission</option>
-                <option value="Faculties">Faculties</option>
-                <option value="Cashier">Cashier</option>
-                <option value="Registrar">Registrar</option>
+               <option value="" disabled {{ old('categorySelection') ? '' : 'selected' }}>Select Category</option>
+               <option value="Admission" {{ old('categorySelection') == 'Admission' ? 'selected' : '' }}>Admission</option>
+               <option value="Faculties" {{ old('categorySelection') == 'Faculties' ? 'selected' : '' }}>Faculties</option>
+               <option value="Cashier"  {{ old('categorySelection') == 'Cashier' ? 'selected' : '' }}>Cashier</option>
+               <option value="Registrar" {{ old('categorySelection') == 'Registrar' ? 'selected' : '' }}>Registrar</option>
             </select>
+            @error('categorySelection')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+
             <select name="priorityLevel" class="input mt-2">
-                <option value="" disabled selected>Select Priority Level</option>
+                <option value="{{ old('priorityLevel') }}" disabled selected>Select Priority Level</option>
                 <option value="low">Low</option>
                 <option value="mid">Mid</option>
                 <option value="high">High</option>
             </select>
+            @error('priorityLevel')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+
             <button class="btn btn-neutral mt-4">Submit</button>
-        </form>
-    </fieldset>
+        </fieldset>
+    </form>
 </x-layout>
 
