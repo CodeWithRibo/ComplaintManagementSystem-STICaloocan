20321111dec7d5e8a6a6f4b3187e279454ac6856
CodeWithRibo <riboluna2818@gmail.com>
2025-06-08 19:42:25 +0800

    Added Model Route Binding

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComplaintController;
 
 Route::get('/', [ComplaintController::class, 'index'])->name('index');
+
+Route::get('SubmitForm',[ComplaintController::class, 'create'])->name('create');
+Route::post('SubmitForm',[ComplaintController::class, 'store'])->name('store');
+
+Route::get('View/{complaint}',[ComplaintController::class, 'show'])->name('show');
