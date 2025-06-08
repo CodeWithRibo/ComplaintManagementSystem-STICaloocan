89836d72fcba85fdee9e6df4abb34ffcf78e2c23
CodeWithRibo <riboluna2818@gmail.com>
2025-06-08 19:42:42 +0800

    Initial Commit

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Complaint;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ComplaintController extends Controller
 {
@@ -13,26 +15,37 @@ class ComplaintController extends Controller
 
     public function index()
     {
-        $query = Complaint::query();
-        $complaint = $query->orderBy('categorySelection','ASC')->paginate(15);
-        return view('welcome',['complaints' => $complaint]);
-
+          $query = Complaint::query();
+          $complaint = $query
+              ->orderBy('title','ASC')
+              ->paginate(20);
+          return view('welcome',['complaints' => $complaint]);
     }
 
     public function create()
     {
-        //
+        return view('ComplaintForm');
     }
 
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'title' => 'required|string|max:255',
+            'description'=>'required|string|min:10|max:300',
+            'categorySelection' => 'required',
+            'priorityLevel' => 'required',
+            'timeIncident' => 'required'
+        ]);
+        $query = Complaint::query();
+        $query -> create($validate);
+
+        redirect('/');
     }
 
 
-    public function show(string $id)
+    public function show(Complaint $complaint)
     {
-        //
+        return view('View',['lists' => $complaint]);
     }
 
     public function edit(string $id)
