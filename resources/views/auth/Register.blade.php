a29ccd930dc7f224ba5e76fecf05967131197c6e
CodeWithRibo <riboluna2818@gmail.com>
2025-06-13 17:59:20 +0800

    Added error message

--- resources/views/auth/Register.blade.php
+++ resources/views/auth/Register.blade.php
@@ -12,12 +12,20 @@
                     <div class="p-6 max-w-7xl mx-auto">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div>
+                            @if($errors->any())
+                                <div role="alert" class="alert alert-error">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{$error}}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                             {{-- First Name --}}
-                           <x-FormLayout type="input" value="{{old('firstName')}}" name="firstName" placeholder="Enter First Name">
+                           <x-FormLayout type="input" value="{{old('first_name')}}" name="first_name" placeholder="Enter First Name">
                                First Name
                            </x-FormLayout>
-                            {{-- Last Name --}}
-                            <x-FormLayout type="input" value="{{old('lastName')}}" name="lastName" placeholder="Enter Last Name">
+                            <x-FormLayout type="input" value="{{old('last_name')}}" name="last_name" placeholder="Enter Last Name">
                                 Last Name
                             </x-FormLayout>
                             {{-- Email --}}
@@ -25,40 +33,40 @@
                                 Email
                             </x-FormLayout>
                             {{-- Password --}}
-                            <x-FormLayout type="password" placeholder="Enter Password">
+                            <x-FormLayout type="password" name="password" placeholder="Enter Password">
                                 Password
                             </x-FormLayout>
                             {{-- Confirm Password --}}
-                            <x-FormLayout type="password" placeholder="Enter Confirm Password">
+                            <x-FormLayout type="password" name="repeat_password" placeholder="Enter Confirm Password">
                                 Confirm Password
                             </x-FormLayout>
                         </div>
                         <div>
                             {{-- Student ID Number --}}
-                            <x-FormLayout type="input" value="{{old('studentIdNumber')}}" name="studentIdNumber" placeholder="Enter Student ID Number">
+                            <x-FormLayout type="input" value="{{old('student_id_number')}}" name="student_id_number" placeholder="Enter Student ID Number">
                                 Student ID Number
                             </x-FormLayout>
                             {{-- Grade Level --}}
-                            <x-SelectionLayout name="gradeLevel" label="Grade Level" disabledOption="Select your Grade Level">
+                            <x-SelectionLayout name="grade_level" label="Grade Level" disabledOption="Select your Grade Level">
                             <option value="1st Year">1st Year</option>
                             <option value="2nd Year">2nd Year</option>
                             <option value="3rd Year">3rd Year</option>
                             <option value="4th Year">4th Year</option>
                             </x-SelectionLayout>
-                            {{-- Program --}}
+                            {{--Program--}}
                             <x-SelectionLayout name="program" label="Program" disabledOption="Select your Program">
                                 <option value="BSIT">BSIT - Information Technology</option>
                                 <option value="BSBA">BSBA - Business Accountancy</option>
                                 <option value="BSHM">BSHM - Hospitality Management</option>
                                 <option value="BSTM">BSTM - Tourism Management</option>
                             </x-SelectionLayout>
-                            {{-- Contact Number --}}
-                            <x-FormLayout type="input"  value="{{old('contactNumber')}}" name="contactNumber" placeholder="Enter Contact Number">
+                            {{--Contact Number--}}
+                            <x-FormLayout type="input"  value="{{old('contact_number')}}" name="contact_number" placeholder="Enter Contact Number">
                                 Contact Number (Optional)
                             </x-FormLayout>
                         </div>
                     </div>
-                        <button class="btn btn-neutral mt-6 w-full">Signup</button>
+                        <button type="submit" class="btn btn-neutral mt-6 w-full">Signup</button>
                     </div>
                 </fieldset>
             </form>
