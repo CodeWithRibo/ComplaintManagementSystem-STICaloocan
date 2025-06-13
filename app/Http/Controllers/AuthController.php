ad18a7f9cbc924c3b2f392f42f492cb21045e832
CodeWithRibo <riboluna2818@gmail.com>
2025-06-13 22:41:15 +0800

    remove remember & email verified database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\AuthRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -24,36 +25,16 @@ class AuthController extends Controller
 
     }
 
-    /**
-     * @throws ValidationException
-     */
-    public  function register(Request $request) : RedirectResponse
+    public  function register(AuthRequest $request) : RedirectResponse
     {
-        $validatedData = Validator::make($request->all(), [
-        'first_name' => ['required', 'string', 'max:50', 'regex:/^[a-zA-Z\s]+$/'],
-        'last_name' => ['required', 'string', 'max:50', 'regex:/^[a-zA-Z\s]+$/'],
-        'email' => ['required', 'email', 'unique:users', 'regex:/^[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/'],
-        'password' => ['required', 'string', 'confirmed:repeat_password'
-        ,Password::min(8)
-                ->letters()
-                ->mixedCase()
-                ->numbers()
-                ->symbols()],
-        'student_id_number' => ['required', 'integer', 'unique:users', 'min:11'],
-        'grade_level' => ['required', 'string'],
-        'program' => ['required', 'string'],
-        'contact_number' => ['nullable', 'integer']
-            //Fixing Validation
-    ])->validated();
-
-//        $validatedData['password'] = bcrypt($validatedData['password']);
+        $validated = $request->validated();
         $query = User::query();
-        $user = $query -> create($validatedData);
-        dd($user);
-//        Auth::login($user);
+        $user = $query -> create($validated);
+        Auth::login($user);
 
-//        return redirect('/');
+        return redirect('/');
     }
+
 
     public  function login()
     {
