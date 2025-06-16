63d27593e45e9d825dfafc0586c489936f17fd11
Carl John Sto. Tomas <riboluna2818@gmail.com>
2025-06-16 22:33:51 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,3 @@
-<x-layout>
-    <x-NavigationBar>
-        <x-slot name="logout">
-            <x-LogoutButton/>
-        </x-slot>
-        <x-slot name="login">
-            <x-LoginButton />
-        </x-slot>
-    </x-NavigationBar>
-    <span class="text-xl text-red-500 font-semibold">Hi there, {{Auth::check() ? Auth::user()->first_name : 'Guest'}}</span>
-</x-layout>
 <x-Layout>
     <x-NavigationBar>
         @if(Auth::guest())
