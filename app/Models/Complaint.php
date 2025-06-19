146c93734a73021ab183cbf829700ddcec31562c
CodeWithRibo <riboluna2818@gmail.com>
2025-06-19 23:10:59 +0800

    added relationship to user

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -4,13 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Complaint extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'categorySelection', 'priorityLevel', 'timeIncident'];
-    protected $table = "complaints";
+    protected $table = 'complaints';
     protected $guarded = "id";
+    protected $fillable = [
+        'title',
+        'description',
+        'categorySelection',
+        'priorityLevel',
+        'timeIncident',
+        'user_id',
+    ];
+    public function user() :BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
