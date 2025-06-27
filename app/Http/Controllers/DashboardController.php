d364585207cbab4c13fa28407bdf560b45abad0f
CodeWithRibo <riboluna2818@gmail.com>
2025-06-27 17:50:32 +0800

    Indicate default status and fetch single row data in views

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Complaint;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function home()
     {
-        return view('dashboard.home');
+        $countPending = auth()->user()
+            ->complaints()
+            ->where('status', 'Pending')
+            ->count();
+
+        $complaintData = auth()->user()->complaints()->orderBy('updated_at', 'desc')->first();
+        return view('dashboard.home', ['countPending' => $countPending, 'complaintData' => $complaintData]);
     }
 
     public function welcome()
