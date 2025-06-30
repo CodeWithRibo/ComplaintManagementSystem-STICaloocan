454e9d4b49bc1789bbfb49fbfc4dcbb02aaaf2ef
CodeWithRibo <riboluna2818@gmail.com>
2025-06-30 19:38:13 +0800

    remove tailwind link

--- resources/views/Components/Layout.blade.php
+++ resources/views/Components/Layout.blade.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
-{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
     <title>CMS</title>
 </head>
 <body class="font-display">
