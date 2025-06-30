25aec7811411c3ff3d7285fc02de094c3f6d7821
CodeWithRibo <riboluna2818@gmail.com>
2025-06-30 17:59:32 +0800

    modify components

--- resources/views/Components/ModalAttachedImage.blade.php
+++ resources/views/Components/ModalAttachedImage.blade.php
@@ -1,8 +1,12 @@
 @props(['label' => ''])
-<div class="md:col-span-2">
-    <label class="block text-xs font-medium text-gray-500 dark:text-gray-400">{{$label}}</label>
-    <div
-        class="mt-2 h-40 rounded bg-gray-100 flex items-center justify-center text-gray-400 text-xs dark:bg-gray-700 dark:text-gray-300">
+
+<div>
+    <label class="block text-sm font-semibold text-gray-700 dark:text-white mb-1">
+        {{ $label }}
+    </label>
+
+    <div class="mt-2 h-40 rounded bg-gray-100 dark:bg-gray-700 dark:text-gray-300 flex items-center justify-center overflow-hidden {{$attributes->get('class')}}">
+        <p class="text-sm text-gray-700 dark:text-gray-300" {{ $attributes }}></p>
         {{$slot}}
     </div>
 </div>
--- resources/views/Components/ModalDetails.blade.php
+++ resources/views/Components/ModalDetails.blade.php
@@ -1,5 +1,5 @@
 @props(['label' => ''])
 <div>
     <label class="block text-xs font-medium text-gray-500 dark:text-gray-400">{{$label}}</label>
-    <p class="text-sm font-semibold text-gray-800 dark:text-white">{{$slot}}</p>
+    <p class="text-sm text-gray-700 dark:text-gray-300" {{ $attributes }}></p>
 </div>
