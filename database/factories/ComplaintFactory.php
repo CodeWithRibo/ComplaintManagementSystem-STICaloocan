983b92fd65676b4f952e2c5c06db5146c6021f66
CodeWithRibo <riboluna2818@gmail.com>
2025-06-30 19:38:43 +0800

    Added fake data

--- database/factories/ComplaintFactory.php
+++ database/factories/ComplaintFactory.php
@@ -19,11 +19,23 @@ class ComplaintFactory extends Factory
     public function definition(): array
     {
         return [
-            'title'=>fake()->word(),
-            'description'=>fake()->realText(),
-            'categorySelection'=>fake()->randomElement(['Facilities','Faculty','Admission','Cashier','Registrar']),
-            'priorityLevel'=>fake()->randomElement(['Low','Mid','High']),
-            'timeIncident'=>fake()->dateTime(),
+            'category'=>fake()->randomElement(['Facilities','Faculty','Admission','Cashier','Registrar']),
+            'location'=>fake()->address(),
+            'title'=>fake()->sentence(6),
+            'description'=>fake()->paragraph(),
+            'incident_time'=>fake()->dateTimeBetween('-1 year', 'now'),
+            'priority'=>fake()->randomElement(['Low', 'Medium', 'High']),
+            'image_path'=>fake()->imageUrl(),
+            'is_anonymous'=>fake()->boolean(),
+            'type_submit'=>fake()->randomElement(['Online', 'In Person']),
+            'full_name'=>fake()->name(),
+            'student_id_number'=>fake()->numerify('20#######'),
+            'email'=>fake()->safeEmail(),
+            'phone_number'=>fake()->phoneNumber(),
+            'year_section'=>fake()->randomElement(['1st Year BSIT BT-107', '2nd Year BSIT BT-307', '3rd 1st Year BSIT BT-607']),
+            'consent_given'=>fake()->boolean(),
+            'status'=>fake()->randomElement(['Pending', 'In Progress', 'Resolved']),
+            'complaint_tracker'=>fake()->uuid(),
         ];
     }
 }
