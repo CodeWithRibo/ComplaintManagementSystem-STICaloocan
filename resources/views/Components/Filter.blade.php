aab970775491ebc15e10d52e7cce07e4cecb1326
CodeWithRibo <riboluna2818@gmail.com>
2025-07-01 02:59:59 +0800

    added attribute slot

--- resources/views/Components/Filter.blade.php
+++ resources/views/Components/Filter.blade.php
@@ -2,7 +2,7 @@
 <div>
 {{--    Filter by Status--}}
     <label for="status" class="block text-sm font-medium text-gray-700 mb-1">{{$label}}</label>
-    <select id="status" class="select select-bordered w-40 text-sm">
+    <select id="status" class="select select-bordered w-40 text-sm" {{$attributes}}>
         {{$slot}}
     </select>
 </div>
