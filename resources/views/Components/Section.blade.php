476f13c86f100b53ebbb9a8f6909c0796b8783ef
CodeWithRibo <riboluna2818@gmail.com>
2025-07-03 21:30:41 +0800

    merge class in the Section.blade.php

--- resources/views/Components/Section.blade.php
+++ resources/views/Components/Section.blade.php
@@ -1,3 +1,4 @@
-<section :class="open ? 'md:ml-[300px]' : 'md:ml-20'" class="pt-16 transition-all duration-300">
+<section
+    :class="open ? 'md:ml-[300px]' : 'md:ml-20'" {{$attributes->merge(['class' => 'pt-16 transition-all duration-300'])}}>
     {{$slot}}
 </section>
