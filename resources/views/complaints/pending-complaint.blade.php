a8b0d6638a7f2d3017c9838ce239eef9daeb6043
CodeWithRibo <riboluna2818@gmail.com>
2025-07-04 02:40:18 +0800

    initial commit

--- resources/views/complaints/pending-complaint.blade.php
+++ resources/views/complaints/pending-complaint.blade.php
@@ -1,6 +1,6 @@
 @php use Illuminate\Support\Carbon; @endphp
 
 @include('Components.complaint-table',[
-'complaints' => $complaintData,
+'complaints' => $pending,
 'header' => 'Pending Complaints'
 ])
