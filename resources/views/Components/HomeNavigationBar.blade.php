2d2a69adc75d31d3c0e2249f9a8c8d2df4574437
CodeWithRibo <riboluna2818@gmail.com>
2025-07-05 01:50:14 +0800

    Modify Icons for Aside Menu

--- resources/views/Components/HomeNavigationBar.blade.php
+++ resources/views/Components/HomeNavigationBar.blade.php
@@ -4,34 +4,35 @@
 
 $mDropdownMenu = [
 'My Complaints' => [
-  ['icon' => 'fa-house', 'label' => 'All Complaints', 'route' => route('dashboard.listComplaint')],
-  ['icon' => 'fa-folder', 'label' => 'Pending Complaints', 'route' => route('complaints.pending')],
-  ['icon' => 'fa-clock', 'label' => 'Resolved Complaints'],
+  ['icon' => 'ph-folder', 'label' => 'All Complaints', 'route' => route('dashboard.listComplaint')],
+  ['icon' => 'ph-hourglass-medium', 'label' => 'Pending Complaints', 'route' => route('complaints.pending')],
+  ['icon' => 'ph-seal-check', 'label' => 'Resolved Complaints', 'route' => route('complaints.resolved')],
 ],
 'Complaint Details' => [
-  ['icon' => 'fa-circle-check', 'label' => 'View / Edit Complaints'],
+   ['icon' => 'ph-pencil-line', 'label' => 'Edit Complaints', 'route' => route('complaints.show')],
 ],
 'Others' => [
-  ['icon' => 'fa-question', 'label' => 'FAQ'],
-  ['icon' => 'fa-address-book', 'label' => 'Contact Us'],
-  ['icon' => 'fa-arrow-right-from-bracket', 'label' => 'Logout'],
+  ['icon' => 'ph-seal-question', 'label' => 'FAQ', 'route' => route('dashboard.faq')],
+  ['icon' => 'ph-address-book-tabs', 'label' => 'Contact Us'],
+  ['icon' => 'ph-sign-out', 'label' => 'Logout', 'route' => route('logout')],
 ],
 ];
 
 $asideMenu = [
 'My Complaints' => [
-  ['icon' => 'fa-house', 'label' => 'All Complaints', 'route' => route('dashboard.listComplaint')],
-  ['icon' => 'fa-folder', 'label' => 'Pending Complaints', 'route' => route('complaints.pending')],
-  ['icon' => 'fa-clock', 'label' => 'Resolved Complaints'],
+  ['icon' => 'ph-folder', 'label' => 'All Complaints', 'route' => route('dashboard.listComplaint')],
+  ['icon' => 'ph-hourglass-medium', 'label' => 'Pending Complaints', 'route' => route('complaints.pending')],
+  ['icon' => 'ph-seal-check', 'label' => 'Resolved Complaints', 'route' => route('complaints.resolved')],
 ],
 'Complaint Details' => [
-  ['icon' => 'fa-circle-check', 'label' => 'Edit Complaints', 'route' => route('complaints.show')],
+  ['icon' => 'ph-pencil-line', 'label' => 'Edit Complaints', 'route' => route('complaints.show')],
 ],
 'Others' => [
-  ['icon' => 'fa-question', 'label' => 'FAQ'],
-  ['icon' => 'fa-address-book', 'label' => 'Contact Us'],
+  ['icon' => 'ph-seal-question', 'label' => 'FAQ', 'route' => route('dashboard.faq')],
+  ['icon' => 'ph-address-book-tabs', 'label' => 'Contact Us'],
 ],
 ];
+
 @endphp
 <div x-data="{ open: false }">
     <div class="navbar fixed z-10 bg-button shadow-sm h-16">
@@ -62,6 +63,14 @@ $asideMenu = [
          x-transition
          class="h-screen fixed top-0 z-0 w-20 bg-white shadow-lg hidden md:flex flex-col items-center pt-10">
         <ul class="w-full flex flex-col items-center gap-5 px-2 pt-10">
+            <li class="flex items-center h-10 gap-3 text-gray-500">
+                <div class="w-10 h-10 flex items-center justify-center">
+                    <a href="{{route('dashboard.home')}}" @class(['text-button font-semibold' => request()->url() === route('dashboard.home'),
+                                                            'text-gray-500' => request()->url() != route('dashboard.home')])>
+                        <i class="ph ph-house text-2xl "></i>
+                    </a>
+                </div>
+            </li>
             @foreach($asideMenu as $header => $items)
                 @foreach($items as $item)
                     <li class="flex items-center justify-center h-10 text-gray-500 hover:text-button">
@@ -69,10 +78,10 @@ $asideMenu = [
                             <a href="{{ $item['route'] }}"
                                 @class(['text-button font-semibold' => request()->url() === $item['route'],
                                          'text-gray-500' => request()->url() != $item['route']])>
-                                <i class="fa-solid {{ $item['icon'] }} text-xl"></i>
+                                <i class="ph {{ $item['icon'] }} text-2xl"></i>
                             </a>
                         @else
-                            <i class="fa-solid {{ $item['icon'] }} text-xl"></i>
+                            <i class="ph {{ $item['icon'] }} text-2xl"></i>
                         @endif
                     </li>
                 @endforeach
@@ -85,17 +94,26 @@ $asideMenu = [
          class="bg-white shadow-lg w-[300px] h-screen hidden md:block overflow-y-auto fixed">
         <div class="text-button flex flex-col">
             <ul class="flex flex-col p-3 pt-20  space-y-3">
-                @foreach($asideMenu as $header => $items)
+                <li class="flex items-center h-10 gap-3 text-gray-500">
+                    <div class="w-10 h-10 flex items-center justify-center">
+                        <i class="ph ph-house text-xl "></i>
+                    </div>
+                    <a href="{{route('dashboard.home')}}" @class(['text-button font-semibold' => request()->url() === route('dashboard.home'),
+                                                                  'text-gray-500' => request()->url() != route('dashboard.home')])>
+                        <h1 class="hover:text-button leading-none flex items-center h-10">Home</h1>
+                    </a>
+                </li>
+            @foreach($asideMenu as $header => $items)
                     <h1 class="text-base-content font-semibold">{{$header}}</h1>
                     @foreach($items as $item)
                         <li class="flex items-center h-10 gap-3 text-gray-500 ">
                             <div class="w-10 h-10 flex items-center justify-center">
-                                <i class="fa-solid {{$item['icon']}} "></i>
+                                <i class="ph {{$item['icon']}} text-xl "></i>
                             </div>
-                            @if(isset($item['route']))
+                        @if(isset($item['route']))
                                 <a href="{{ $item['route'] }}"
-                                   @class(['text-button font-semibold' => request()->url() === $item['route'],
-                                            'text-gray-500' => request()->url() != $item['route']])>
+                                   @class(['text-button  font-semibold' => request()->url() === $item['route'],
+                                            'text-gray-500 hover:text-button' => request()->url() != $item['route']])>
                                     {{ $item['label'] }}
                                 </a>
                             @else
@@ -123,15 +141,32 @@ $asideMenu = [
                     <span
                         class=" text-gray-500 hover:text-button leading-none flex items-center h-10">{{$convert}}</span>
                 </li>
+                <li class="flex items-center h-10 gap-3 text-gray-500">
+                    <div class="w-10 h-10 flex items-center justify-center">
+                        <i class="ph ph-house text-xl "></i>
+                    </div>
+                    <a href="{{route('dashboard.home')}}" @class(['text-button font-semibold' => request()->url() === route('dashboard.home'),
+                                                                  'text-gray-500' => request()->url() != route('dashboard.home')])>
+                        <h1 class="hover:text-button leading-none flex items-center h-10">Home</h1>
+                    </a>
+                </li>
                 @foreach($mDropdownMenu as $header => $items)
                     <h1 class="text-base-content font-semibold">{{$header}}</h1>
                     @foreach($items as $item)
-                        <li class="flex items-center h-10 gap-3 text-gray-500 hover:text-button">
+                        <li class="flex items-center h-10 gap-3 text-gray-500 ">
                             <div class="w-10 h-10 flex items-center justify-center">
-                                <i class="fa-solid {{$item['icon']}} "></i>
+                                <i class="ph {{$item['icon']}} text-xl "></i>
                             </div>
-                            <span
-                                class="text-gray-500 hover:text-button leading-none flex items-center h-10">{{$item['label']}}</span>
+                            @if(isset($item['route']))
+                                <a href="{{ $item['route'] }}"
+                                    @class(['text-button  font-semibold' => request()->url() === $item['route'],
+                                             'text-gray-500 hover:text-button' => request()->url() != $item['route']])>
+                                    {{ $item['label'] }}
+                                </a>
+                            @else
+                                <span
+                                    class="text-gray-500 hover:text-button leading-none flex items-center h-10">{{$item['label']}}</span>
+                            @endif
                         </li>
                     @endforeach
                 @endforeach
