05180fe718f586a4f43dacd47208581fe95c5140
CodeWithRibo <riboluna2818@gmail.com>
2025-07-06 23:12:25 +0800

    Added input border color

--- resources/views/Components/SelectionLayout.blade.php
+++ resources/views/Components/SelectionLayout.blade.php
@@ -3,7 +3,7 @@
     <label class="label">
         <span class="label-text">{{ $label }}</span>
     </label>
-    <select {{ $attributes }} name="{{$name}}" class="@error($name) is-invalid @enderror select select-bordered w-full" required>
+    <select {{ $attributes }} name="{{$name}}" class="@error($name) is-invalid @enderror select select-bordered w-full border-1 focus:border-none focus:outline-none focus:ring focus:ring-blue-400" required>
         <option disabled selected> {{$disabledOption}}</option>
        {{$slot}}
     </select>
