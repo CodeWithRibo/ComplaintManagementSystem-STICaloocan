f881b2a8d10567da936bc19bac30dd84ceca6f0e
CodeWithRibo <riboluna2818@gmail.com>
2025-07-08 22:14:52 +0800

    Added Forgot password logic

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class ForgotPasswordController extends Controller
+{
+    public function showLinkRequestForm()
+    {
+        return view('auth.forgot-password');
+    }
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users,email',
+        ], [
+            'email.exists' => 'We couldn’t find an account with that email.',
+        ]);
+
+        try {
+            $token = Str::random(40);
+
+            DB::table('password_reset_tokens')->where('email', $request->email)->delete();
+            DB::table('password_reset_tokens')->insert([
+                'email' => $request->email,
+                'token' => $token,
+                'created_at' => Carbon::now()
+            ]);
+
+            //Send Email
+            Mail::send('auth.verify', ['token' => $token], function ($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Reset Your Password');
+            });
+            return back()->with('success', 'We have e-mailed your password reset link!');
+
+        }catch (Exception $e) {
+            Log::error($e->getMessage());
+            return back()->with('error', 'Something went wrong while sending the reset email. Please try again.');
+        }
+    }
+}
