075031c4519cb40cf8653b184ed3b2d810a5daba
CodeWithRibo <riboluna2818@gmail.com>
2025-07-12 20:52:42 +0800

    Added route admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,14 @@ use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\user\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show-login');
+// ----------------------------- Register --------------------------//
+Route::get('/register', [AuthController::class, 'showRegister'])->name('admin.show-register');
+Route::post('/register', [AuthController::class, 'register'])->name('admin.register');
+// ----------------------------- Login --------------------------//
+Route::get('/login', [AuthController::class, 'showLogin'])->name('admin.show-login');
+Route::post('/login', [AuthController::class, 'login'])->name('admin.login');
+// ----------------------------- Dashboard --------------------------//
+Route::get('/dashboard', function () {
+    $name = "CarlJohn";
+    return view('admin.dashboard.index', ['name' => $name]);
+})->name('admin.index');
