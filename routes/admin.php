e32459383e442bbb3d899d5676500a727bb2d14c
CodeWithRibo <riboluna2818@gmail.com>
2025-07-14 21:21:58 +0800

    Fixed customize admin guest and auth for middleware

--- routes/admin.php
+++ routes/admin.php
@@ -1,17 +1,21 @@
 <?php
 
-use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\AdminAuthController;
 use App\Http\Controllers\user\DashboardController;
 use Illuminate\Support\Facades\Route;
-
+Route::middleware(['admin.guest'])->group( function (){
 // ----------------------------- Register --------------------------//
-Route::get('/register', [AuthController::class, 'showRegister'])->name('admin.show-register');
-Route::post('/register', [AuthController::class, 'register'])->name('admin.register');
+Route::get('/register', [AdminAuthController::class, 'showRegister'])->name('admin.show-register');
+Route::post('/register', [AdminAuthController::class, 'register'])->name('admin.register');
 // ----------------------------- Login --------------------------//
-Route::get('/login', [AuthController::class, 'showLogin'])->name('admin.show-login');
-Route::post('/login', [AuthController::class, 'login'])->name('admin.login');
+Route::get('/login', [AdminAuthController::class, 'showLogin'])->name('admin.show-login');
+Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login');
 // ----------------------------- Dashboard --------------------------//
+});
+
+Route::middleware(['admin.auth'])->group(function () {
 Route::get('/dashboard', function () {
     $name = "CarlJohn";
     return view('admin.dashboard.index', ['name' => $name]);
 })->name('admin.index');
+});
