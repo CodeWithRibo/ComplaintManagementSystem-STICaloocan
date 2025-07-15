31b76c346c42900755bb4fe6fa1c893357470e9e
CodeWithRibo <riboluna2818@gmail.com>
2025-07-15 17:47:34 +0800

    Added validation in request

--- app/Http/Controllers/admin/AdminAuthController.php
+++ app/Http/Controllers/admin/AdminAuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\admin\AdminUser;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Validation\Rules\Password;
 
 class AdminAuthController extends Controller
 {
@@ -23,29 +24,49 @@ class AdminAuthController extends Controller
     public function register(Request $request)
     {
         $validate = $request->validate([
-            'user_name' => 'required|string',
-            'password' => 'required|string|confirmed:repeat_password',
+            'first_name' => ['required', 'string', 'min:3', 'max:50', 'regex:/^[a-zA-Z\s]+$/'],
+            'last_name' => ['required', 'string', 'min:2',  'max:50', 'regex:/^[a-zA-Z\s.-]+$/'],
+            'email' => ['required', 'email', 'unique:admin_users', 'regex:/^[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/'],
+            'password' => [
+                'required',
+                'string',
+                'confirmed:repeat_password',
+                Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+            ],
+            'contact_number' => ['nullable', 'max:11', 'regex:/^[0-9]+$/'],
         ]);
 
         $query = AdminUser::query();
         $query->create($validate);
-
-        return redirect('admin/login');
+        noty()
+            ->timeout(2000)
+            ->theme('relax')
+            ->success('Registration complete! You can now log in to your account.');
+        return redirect()->route('admin.show-login');
     }
 
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'user_name' => 'required',
+            'email' => 'required',
             'password' => 'required',
         ]);
 
         if(Auth::guard('admin')->attempt($credentials))
         {
             $request->session()->regenerate();
+            $name = Auth::guard('admin')->user()->first_name;
+            noty()
+                ->timeout(2000)
+                ->theme('relax')
+                ->success("Hello $name, you're now logged in.");
             return redirect()->route('admin.index');
         }
-        return back()->withErrors(['user_name' => 'Your user name or password is incorrect.']);
+        return back()->withErrors(['email' => 'Your email or password is incorrect.']);
     }
 
 }
