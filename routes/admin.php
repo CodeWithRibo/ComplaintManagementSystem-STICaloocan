4f6b4169a62bade38676f10d994a591c0699bac2
CodeWithRibo <riboluna2818@gmail.com>
2025-07-17 19:59:53 +0800

    Intial commit

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminAuthController;
+use App\Http\Controllers\admin\AdminDashboardController;
 use App\Http\Controllers\user\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -10,22 +11,21 @@ Route::get('/', function () {
         : redirect()->route('admin.show-login');
 });
 Route::middleware(['admin.guest'])->group( function (){
-// ----------------------------- Register --------------------------//
+    // ----------------------------- Register --------------------------//
 Route::get('/register', [AdminAuthController::class, 'showRegister'])->name('admin.show-register');
 Route::post('/register', [AdminAuthController::class, 'register'])->name('admin.register');
-// ----------------------------- Login --------------------------//
+    // ----------------------------- Login --------------------------//
 Route::get('/login', [AdminAuthController::class, 'showLogin'])->name('admin.show-login');
-Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login');
-// ----------------------------- Dashboard --------------------------//
-});
-Route::middleware(['admin.auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        $name = "CarlJohn";
-        return view('admin.dashboard.index', ['name' => $name]);
-    })->name('admin.index');
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login');
 });
 
-// ----------------------------- Logout --------------------------//
+Route::middleware(['admin.auth'])->group(function () {
+    // ----------------------------- Dashboard --------------------------//
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.index');
+
+});
+
+    // ----------------------------- Logout --------------------------//
 Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 Route::get('/logout', function () {
     Auth::guard('admin')->logout();
