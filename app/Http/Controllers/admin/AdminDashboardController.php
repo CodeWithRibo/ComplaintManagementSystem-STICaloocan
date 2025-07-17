c5684104b17f80083dd5d65a32b9b01029c956b4
CodeWithRibo <riboluna2818@gmail.com>
2025-07-17 20:01:18 +0800

    Added AdminDashboard Logic

---
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\user\Complaint;
+use App\Models\user\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminDashboardController extends Controller
+{
+    public function index()
+    {
+        $userComplaints = Complaint::with('user')->take(5)->get();
+        return view('admin.dashboard.index', compact('userComplaints') );
+    }
+}
