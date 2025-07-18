fe9a6dc72372017136f674aed7d64c1b72566ad2
CodeWithRibo <riboluna2818@gmail.com>
2025-07-18 08:28:00 +0800

    Modify status using switch

--- resources/views/Components/complaint-table.blade.php
+++ resources/views/Components/complaint-table.blade.php
@@ -30,7 +30,7 @@
                             @if(!empty($resolutionNote))
                                 <th class="px-6 py-3 rounded-tr-md">{{$resolutionNote}}</th>
                             @endif
-                            <th class="px-6 py-3 rounded-tr-md">Details</th>
+                            <th class="px-6 py-3 rounded-tr-md">{{!empty($action) ? 'Action' : 'Details'}}</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -38,38 +38,52 @@
                             <tr class="bg-white shadow-sm hover:shadow-md transition rounded-md">
                                 <td class="px-6 py-4 font-medium text-gray-800">{{$data->title}}</td>
                                 <td class="px-6 py-4">
-                                    @if($data->status == 'Resolved')
-                                        <span class="inline-flex items-center gap-2 text-xs font-semibold text-green-800 bg-green-100 border border-green-300 px-3 py-1 rounded-full">
-                                        <i class="fa-solid fa-check text-green-600 text-sm"></i>
-                                        <span class="text-sm">Resolved</span>
-                                    </span>
-                                    @else
-                                        <span
-                                            class="inline-flex items-center gap-1 text-xs font-semibold text-yellow-800 bg-yellow-100 px-3 py-1 rounded-full">
-                                    <i class="fa-solid fa-spinner animate-spin text-yellow-600"></i>
-                                      <span class="font-semibold text-sm">Pending</span>
-                                </span>
-                                    @endif
+                                    @switch($data)
+                                        @case($data->status === 'Pending')
+                                            <x-StatusLayout label="Pending" icon="pending"
+                                                            class="bg-yellow-100 text-yellow-800"/>
+                                            @break
+                                        @case($data->status === 'Resolved')
+                                            <x-StatusLayout label="Resolved" icon="resolved"
+                                                            class="bg-green-100 text-green-800"/>
+                                            @break
+                                        @case($data->status === 'In Progress')
+                                            <x-StatusLayout label="In Progress" icon="resolved"
+                                                            class="bg-blue-100 text-blue-800"/>
+                                            @break
+                                        @case($data->status === 'Archive')
+                                            <x-StatusLayout label="Archive" icon="archive"
+                                                            class="bg-gray-100 text-gray-800"/>
+                                            @break
+                                    @endswitch
                                 </td>
                                 <td class="px-6 py-4 text-xs font-semibold text-gray-700">{{$data->priority}}</td>
                                 <td class="px-6 py-4 text-[13px] text-gray-500">{{Carbon::parse($data->created_at)->format('F jS, Y g:i A')}}</td>
                                 <td class="px-6 py-4 italic text-[13px] text-gray-700">{{$data->complaint_tracker}}</td>
                                 @if(!empty($resolutionNote))
-                                    <td class="px-6 py-4 italic text-[13px] text-gray-700">Lorem ipsumm</td>
+                                    <td class="px-6 py-4 italic text-[13px] text-gray-700">{{$data->resolution_note}}</td>
                                 @endif
                                 <td>
-                                    <button class="btn btn-secondary px-5"
-                                            @click="modalIsOpen = true; selectedComplaint = {{json_encode($data)}} "
-                                            type="button">View Complaint
-                                    </button>
+                                    @if(!empty($action))
+                                        <a href="{{route('complaints.edit', $data->id)}}">
+                                            <button class="btn btn-secondary px-5 text-white"
+                                                    type="button">Edit Complaint
+                                            </button>
+                                        </a>
+                                    @else
+                                        <button class="btn btn-secondary px-5 text-white"
+                                                @click="modalIsOpen = true; selectedComplaint = {{json_encode($data)}} "
+                                                type="button">View Complaint
+                                        </button>
+                                    @endif
                                 </td>
-                            @empty
-                                @if(request()->has('search'))
+                                @empty
+                                    @if(request()->has('search'))
                                         <td colspan="6" class="text-2xl text-gray-500 text-center italic">No Complaint
                                             found for <span class="text-accent">{{request()->get('search')}}</span></td>
                                     @else
                                         <td colspan="6" class="text-2xl text-gray-500 text-center italic">
-                                            No complaints have been submitted yet.
+                                            {{$noComplaintFound}}
                                         </td>
                                     @endif
                             </tr>
@@ -77,7 +91,7 @@
                         </tbody>
                     </table>
                     {{ $complaints->links() }}
-{{--Modal View Details--}}
+                    {{--Modal View Details--}}
                     <x-view-details-modal/>
                 </div>
             </div>
