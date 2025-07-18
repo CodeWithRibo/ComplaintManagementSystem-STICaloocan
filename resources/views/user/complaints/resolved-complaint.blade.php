252ae623e9adf2b0b829bcb568604adb0a09c574
CodeWithRibo <riboluna2818@gmail.com>
2025-07-18 08:30:22 +0800

    Modification for if the user not submit complaint

--- resources/views/user/complaints/resolved-complaint.blade.php
+++ resources/views/user/complaints/resolved-complaint.blade.php
@@ -1,7 +1,8 @@
 @include('Components.complaint-table',
 ['complaints' => $resolved,
 'header' => 'Resolved Complaint',
-'resolutionNote' => 'Resolution Note'
+'resolutionNote' => 'Resolution Note',
+'noComplaintFound' => 'No complaints have been resolved yet.',
 ])
 
 @include('Components.AuthFooter')
