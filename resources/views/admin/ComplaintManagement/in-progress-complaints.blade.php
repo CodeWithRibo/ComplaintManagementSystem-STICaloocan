35f22ca85b8e4ebf2643f9f24e0d34a47a8c50bf
CodeWithRibo <riboluna2818@gmail.com>
2025-07-20 14:44:12 +0800

    initial commit

--- resources/views/admin/ComplaintManagement/in-progress-complaints.blade.php
+++ resources/views/admin/ComplaintManagement/in-progress-complaints.blade.php
@@ -1,18 +1,18 @@
 <x-Layout>
     <x-admin.navigation-bar>
         <x-Section>
-            <div class=" mt-20">
-                <x-Search header="All Complaints"></x-Search>{{--SEARCH BAR, UNDER DEVELOPMENT--}}
-                <div x-data="{modalIsOpen: false, selectedComplaint: null}">
-                    @include('Components.admin.complaint-table',
-                    ['complaints' => $progress,
-                     'resolutionNote' => null,
-                     'noComplaintFound' => 'No complaints are currently in progress'
-                      ])
-                    <x-view-details-modal/>
-                </div>
+            <div class="flex flex-col bg-white shadow-md rounded-md px-5 py-5 mt-20 mx-10"
+                 x-data="{modalIsOpen: false, selectedComplaint: null}">
+                <x-Search></x-Search>{{--SEARCH BAR, UNDER DEVELOPMENT--}}
+                <h1 class="text-base-content text-2xl p-2 my-5 text-center xl:text-start border-l-4 border-primary bg-base-100 shadow-sm rounded-r-lg w-auto xl:w-96">In Progress Complaints</h1>
+                @include('Components.admin.complaint-table',
+                               ['complaints' => $progress,
+                                'resolutionNote' => null,
+                                'noComplaintFound' => 'No complaints are currently in progress'
+                                 ])
+                <x-view-details-modal/>
             </div>
-            @include('Components.AuthFooter')
+
         </x-Section>
     </x-admin.navigation-bar>
 </x-Layout>
