ba76d1a858600d0a6d5183efd459cde0332307b2
CodeWithRibo <riboluna2818@gmail.com>
2025-07-20 14:42:33 +0800

    added user management

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\admin\AdminAuthController;
 use App\Http\Controllers\admin\AdminDashboardController;
 use App\Http\Controllers\admin\ComplaintManagementController;
+use App\Http\Controllers\admin\UserManagementController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
@@ -21,21 +22,25 @@ Route::get('/login', [AdminAuthController::class, 'showLogin'])->name('admin.sho
 
 Route::middleware(['admin.auth'])->group(function () {
     // ----------------------------- Complaint Management --------------------------//
-    Route::get('/all-complaints', [ComplaintManagementController::class, 'allComplaints'])->name('admin.all-complaints');
-    Route::get('/pending-complaints', [ComplaintManagementController::class, 'showPending'])->name('admin.pending-complaints');
-    Route::get('/in-progress-complaints', [ComplaintManagementController::class, 'showInProgress'])->name('admin.in-progress-complaints');
-    Route::get('/resolved-complaints', [ComplaintManagementController::class, 'showResolved'])->name('admin.resolved-complaints');
-    Route::get('/archived-complaints', [ComplaintManagementController::class, 'showArchived'])->name('admin.archived-complaints');
-
-    Route::get('/resolution-note/{complaint}', [ComplaintManagementController::class, 'showResolutionNote'])->name('admin.show-resolution-note');
-    Route::put('/resolution-note/{complaint}', [ComplaintManagementController::class, 'resolutionNote'])->name('admin.resolution-note');
-    Route::get('/progress-note/{complaint}', [ComplaintManagementController::class, 'showProgressNote'])->name('admin.show-progress-note');
-    Route::put('/progress-note/{complaint}', [ComplaintManagementController::class, 'progressNote'])->name('admin.progress-note');
-    Route::put('/in-progress/{complaint}', [ComplaintManagementController::class, 'inProgress'])->name('admin.in-progress');
-    Route::put('/resolved/{complaint}', [ComplaintManagementController::class, 'resolved'])->name('admin.resolved');
-    Route::put('/archived/{complaint}', [ComplaintManagementController::class, 'archived'])->name('admin.archived');
-    Route::get('/edit-complaint/{complaint}', [ComplaintManagementController::class, 'showEditComplaint'])->name('admin.show-edit-complaint');
-    Route::post('/edit-complaint/{complaint}', [ComplaintManagementController::class, 'editComplaint'])->name('admin.edit-complaint');
+    Route::get('all-complaints', [ComplaintManagementController::class, 'allComplaints'])->name('admin.all-complaints');
+    Route::get('pending-complaints', [ComplaintManagementController::class, 'showPending'])->name('admin.pending-complaints');
+    Route::get('in-progress-complaints', [ComplaintManagementController::class, 'showInProgress'])->name('admin.in-progress-complaints');
+    Route::get('resolved-complaints', [ComplaintManagementController::class, 'showResolved'])->name('admin.resolved-complaints');
+    Route::get('archived-complaints', [ComplaintManagementController::class, 'showArchived'])->name('admin.archived-complaints');
+    Route::get('resolution-note/{complaint}', [ComplaintManagementController::class, 'showResolutionNote'])->name('admin.show-resolution-note');
+    Route::put('resolution-note/{complaint}', [ComplaintManagementController::class, 'resolutionNote'])->name('admin.resolution-note');
+    Route::get('progress-note/{complaint}', [ComplaintManagementController::class, 'showProgressNote'])->name('admin.show-progress-note');
+    Route::put('progress-note/{complaint}', [ComplaintManagementController::class, 'progressNote'])->name('admin.progress-note');
+    Route::put('in-progress/{complaint}', [ComplaintManagementController::class, 'inProgress'])->name('admin.in-progress');
+    Route::put('resolved/{complaint}', [ComplaintManagementController::class, 'resolved'])->name('admin.resolved');
+    Route::put('archived/{complaint}', [ComplaintManagementController::class, 'archived'])->name('admin.archived');
+    Route::get('edit-complaint/{complaint}', [ComplaintManagementController::class, 'showEditComplaint'])->name('admin.show-edit-complaint');
+    Route::post('edit-complaint/{complaint}', [ComplaintManagementController::class, 'editComplaint'])->name('admin.edit-complaint');
+    // ----------------------------- User Management--------------------------//
+    Route::get('/student-accounts', [UserManagementController::class, 'showStudentAccount'])->name('admin.student-accounts');
+    Route::get('/admin-accounts', [UserManagementController::class, 'showAdminAccount'])->name('admin.admin-accounts');
+    Route::get('/user-profile/{user}', [UserManagementController::class, 'showUserProfile'])->name('admin.show-user-profile');
+    Route::put('/user-profile/{user}', [UserManagementController::class, 'editUserProfile'])->name('admin.user-profile');
     // ----------------------------- Overview & Reports --------------------------//
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.index');
 
