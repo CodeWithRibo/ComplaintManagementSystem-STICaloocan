6e2365f5daabaa67f3454090bd9b153b8e55e0b0
CodeWithRibo <riboluna2818@gmail.com>
2025-07-20 14:45:40 +0800

    added forgot password link

--- resources/views/Components/login-layout.blade.php
+++ resources/views/Components/login-layout.blade.php
@@ -16,6 +16,7 @@
                     <x-FormLayout type="password" name="password" placeholder="Enter Password" label="Password"
                                   x-bind:type="passwordShow ? 'text' : 'password'" class="relative z-0"/>
                     <div class="flex items-center gap-2">
+                        <a href="{{route('show.link-request-form')}}" class="text-gray-700 pr-2 underline ">Forgot Password</a>
                         <input type="checkbox" class="cursor-pointer" @click="passwordShow = !passwordShow" >
                         <p class="text-gray-500">Show Password</p>
                     </div>
