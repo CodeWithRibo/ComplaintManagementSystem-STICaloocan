9e71ed8a5348ced6c7b729f40d0a5bc044c62d17
CodeWithRibo <riboluna2818@gmail.com>
2025-07-21 23:22:04 +0800

    initial commit

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\admin\AdminAuthController;
 use App\Http\Controllers\admin\AdminDashboardController;
 use App\Http\Controllers\admin\ComplaintManagementController;
 use App\Http\Controllers\admin\UserManagementController;
+use App\Http\Controllers\user\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
@@ -37,10 +38,18 @@ Route::middleware(['admin.auth'])->group(function () {
     Route::get('edit-complaint/{complaint}', [ComplaintManagementController::class, 'showEditComplaint'])->name('admin.show-edit-complaint');
     Route::post('edit-complaint/{complaint}', [ComplaintManagementController::class, 'editComplaint'])->name('admin.edit-complaint');
     // ----------------------------- User Management--------------------------//
-    Route::get('/student-accounts', [UserManagementController::class, 'showStudentAccount'])->name('admin.student-accounts');
-    Route::get('/admin-accounts', [UserManagementController::class, 'showAdminAccount'])->name('admin.admin-accounts');
-    Route::get('/user-profile/{user}', [UserManagementController::class, 'showUserProfile'])->name('admin.show-user-profile');
-    Route::put('/user-profile/{user}', [UserManagementController::class, 'editUserProfile'])->name('admin.user-profile');
+    Route::get('student-accounts', [UserManagementController::class, 'showStudentAccount'])->name('admin.student-accounts');
+    Route::get('admin-accounts', [UserManagementController::class, 'showAdminAccount'])->name('admin.admin-accounts');
+    Route::get('student-accounts/user-profile/{user}', [UserManagementController::class, 'showUserProfile'])->name('admin.show-user-profile');
+    Route::put('student-accounts/user-profile/{user}', [UserManagementController::class, 'editUserProfile'])->name('admin.user-profile');
+    Route::post('student-accounts/change-password/{user}', [UserManagementController::class, 'userChangePassword'])->name('admin.user-change-password');
+    Route::get('student-accounts/register',[AuthController::class, 'showRegister'])->name('show.register');
+    Route::post('student-accounts/register',[AuthController::class, 'register'])->name('register');
+    Route::delete('student-accounts/destroy/{user}',[UserManagementController::class, 'destroy'])->name('destroy');
+    Route::delete('admin-accounts/destroy/{adminUser}',[UserManagementController::class, 'destroyAdminAccount'])->name('destroy.admin-account');
+    Route::get('admin-accounts/admin-profile/{adminUser}',[UserManagementController::class, 'showAdminProfile'])->name('admin.show-admin-profile');
+    Route::put('admin-accounts/admin-profile/{adminUser}',[UserManagementController::class, 'editAdminProfile'])->name('admin.admin-profile');
+    Route::post('admin-accounts/change-password/{adminUser}', [UserManagementController::class, 'adminChangePassword'])->name('admin.admin-change-password');
     // ----------------------------- Overview & Reports --------------------------//
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.index');
 
