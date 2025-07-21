16fc539f7ce6799f95bdbc8e12350175311ec393
CodeWithRibo <riboluna2818@gmail.com>
2025-07-21 23:23:14 +0800

    initial commit

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,6 +2,7 @@
     'action' => route('admin.login'),
     'header' => 'Login Admin Account',
     'link' => route('admin.register'),
+    'adminAccess' => true,
     'name' => 'email',
     'label' => 'Email',
     'placeholder' => 'Enter email',
--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -1,7 +1,7 @@
 @include('Components.login-layout', [
     'action' => route('login'),
     'header' => 'Login',
-    'link' => route('show.register'),
+    'adminAccess' => null,
     'name' => 'student_id_number',
     'label' => 'Student ID Number',
     'placeholder' => 'Enter Student ID Number',
