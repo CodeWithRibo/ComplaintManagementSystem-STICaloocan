e93193175f980ce1d6ba7681e1fc8d2889b46cac
CodeWithRibo <riboluna2818@gmail.com>
2025-07-21 23:22:17 +0800

    initial commit

--- resources/views/admin/ComplaintManagement/all-complaints.blade.php
+++ resources/views/admin/ComplaintManagement/all-complaints.blade.php
@@ -3,7 +3,7 @@
         <x-Section>
             <div class="flex flex-col bg-white shadow-md rounded-md px-5 py-5 mt-20 mx-10"
                  x-data="{modalIsOpen: false, selectedComplaint: null}">
-                <x-Search></x-Search>{{--SEARCH BAR, UNDER DEVELOPMENT--}}
+
                 <h1 class="text-base-content text-2xl p-2 my-5 text-center xl:text-start border-l-4 border-primary bg-base-100 shadow-sm rounded-r-lg w-auto xl:w-96">All Complaints</h1>
                 @include('Components.admin.complaint-table',
                                ['complaints' => $allUserComplaints,
