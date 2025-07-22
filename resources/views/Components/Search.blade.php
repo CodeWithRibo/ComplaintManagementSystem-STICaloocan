9ad4e736242ce1fb8b978e1ebf4f521bf6022063
CodeWithRibo <riboluna2818@gmail.com>
2025-07-22 21:20:35 +0800

    Refactor Search Filter

--- resources/views/Components/Search.blade.php
+++ resources/views/Components/Search.blade.php
@@ -3,7 +3,7 @@
 <!-- 🔍 Filters with Search Bar -->
 <div class="flex flex-wrap justify-between items-center gap-4 mb-4 px-4 pt-5">
     <div class="order-1">
-        <h2 class="text-base-content text-2xl text-center py-2">{{$header}}</h2>
+        <h1 class="text-base-content text-2xl p-2 my-5 text-center xl:text-start border-l-4 border-primary bg-base-100 shadow-sm rounded-r-lg w-auto xl:w-96">{{$header}}</h1>
     </div>
     <!-- Search Input -->
     <div class="w-full md:w-1/3 order-2">
